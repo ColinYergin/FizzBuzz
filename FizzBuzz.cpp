7dbcdcfec2cc79d55d1a43c74d8d1e51053da6b7
ColinYergin <yergicol@umich.edu>
2015-04-06 23:46:36 -0400

    Some comments

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -10,18 +10,24 @@ bool IsMult(int a, int b) {
 int main() {
 	for(int i = 1; i <= 100; i++) {
 		if(IsMult(i, 3)) {
+			//Multiple of 3
 			cout << "Fizz";
 			if (IsMult(i, 5)) {
+				//Multiple of 5 too
 				cout << "Buzz";
 			}
 		} else {
 			if (IsMult(i, 5)) {
+				//Multiple of only 5
 				cout << "Buzz";
 			} else {
+				//Not a multiple of either
 				cout << i;
 			}
 		}
+		
 		cout << endl;
 	}
+	
 	return 0;
 }
